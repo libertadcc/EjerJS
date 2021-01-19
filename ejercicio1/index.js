48cff922ec70f27d205f058a8c22203dab311372
libertadcc <libertadchapinalcervantes@gmail.com>
2021-01-19 18:52:12 +0100

    ejercicio1 updated

--- ejercicio1/index.js
+++ ejercicio1/index.js
@@ -9,3 +9,14 @@ const areaLateral = 23 *13;
 const areaBase = 16*23;
 const areaTotal = areaLateral + 2*areaBase;
 console.log('areaTotal', areaTotal + ' cm2');
+
+// Generalizar las formulas
+const base = 25;
+const altura = 10;
+const ancho = 6;
+
+const volumen2 = base * altura * ancho;
+console.log(volumen2);
+
+const area2 = base * altura + 2 * ancho * base;
+console.log('area2', area2)
